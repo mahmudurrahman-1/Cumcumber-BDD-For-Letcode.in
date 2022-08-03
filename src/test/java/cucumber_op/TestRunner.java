bb42e272ccf40e753fe8853389c59dd74b1eec64
mahmudurrahman-1 <mahmudur.rahman.sqa@gmail.com>
2022-08-03 22:08:09 +0600

    issues here

--- src/test/java/cucumber_op/TestRunner.java
+++ src/test/java/cucumber_op/TestRunner.java
@@ -5,7 +5,7 @@ import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(
         features = "login/login.feature",
-        glue = "Hook",
+        glue = "",
         plugin = {"pretty", "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:",
                 "timeline:test-output-thread/", "rerun:target/failedrerun.txt"}
 )
