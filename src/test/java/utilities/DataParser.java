de60ab348c5f4631b844446cdeb2a77e3f5a2fff
mahmudurrahman-1 <mahmudur.rahman.sqa@gmail.com>
2022-08-03 22:06:04 +0600

    data loaded

--- src/test/java/utilities/DataParser.java
+++ src/test/java/utilities/DataParser.java
@@ -1,10 +1,10 @@
 package utilities;
 
 import java.io.File;
-        import java.io.FileInputStream;
-        import java.io.FileNotFoundException;
-        import java.io.IOException;
-        import java.util.Properties;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
 
 public class DataParser {
     public static Properties loadProperties(String file_name) {
