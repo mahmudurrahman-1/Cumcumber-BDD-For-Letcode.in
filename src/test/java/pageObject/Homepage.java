29bed8bae181a997651ee5dfa4a903295590d043
mahmudurrahman-1 <mahmudur.rahman.sqa@gmail.com>
2022-08-03 22:06:17 +0600

    button added

--- src/test/java/pageObject/Homepage.java
+++ src/test/java/pageObject/Homepage.java
@@ -6,30 +6,40 @@ import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import utilities.Common;
 
+import java.util.List;
+
 public class Homepage extends Common {
-    public Homepage(){
-        PageFactory.initElements(PageDriver.getCurrentDriver(),this);
+    public Homepage() {
+        PageFactory.initElements(PageDriver.getCurrentDriver(), this);
     }
+
     /*******************
      * Locators
      */
-    @FindBy(xpath="//a[contains(text(),'Sign up')]")
+    @FindBy(xpath = "//a[contains(text(),'Sign up')]")
     public WebElement signupbutton;
-    @FindBy(xpath="//a[contains(text(),'Log in')]")
+    @FindBy(xpath = "//a[contains(text(),'Log in')]")
     public WebElement loginbutton;
-    @FindBy(xpath="//a[contains(text(),'Sign out')]")
+    @FindBy(xpath = "//a[contains(text(),'Sign out')]")
     public WebElement signoutbutton;
+    @FindBy(xpath = "//a[contains(text(),'Sign out')]")
+    List<WebElement> signoutbuttonsize;
 
     /********************
      * Action goes here
      */
-    public void signup(){
+    public void signup() {
         signupbutton.click();
     }
-    public void login(){
+
+    public void login() {
         loginbutton.click();
     }
-    public void signout(){
+
+    public void signout() {
         signoutbutton.click();
+    }
+    public int signOutbutton(){
+       return signoutbuttonsize.size();
     }
 }
