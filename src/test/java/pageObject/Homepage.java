9d0a7fc9447fbe87d88b0d56a40bfb3e2a24b4e9
mahmudurrahman-1 <mahmudur.rahman.sqa@gmail.com>
2022-08-03 08:38:44 +0600

    home updated

--- src/test/java/pageObject/Homepage.java
+++ src/test/java/pageObject/Homepage.java
@@ -1,6 +1,8 @@
 package pageObject;
 
 import driver.PageDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import utilities.Common;
 
@@ -8,5 +10,26 @@ public class Homepage extends Common {
     public Homepage(){
         PageFactory.initElements(PageDriver.getCurrentDriver(),this);
     }
+    /*******************
+     * Locators
+     */
+    @FindBy(xpath="//a[contains(text(),'Sign up')]")
+    public WebElement signupbutton;
+    @FindBy(xpath="//a[contains(text(),'Log in')]")
+    public WebElement loginbutton;
+    @FindBy(xpath="//a[contains(text(),'Sign out')]")
+    public WebElement signoutbutton;
 
+    /********************
+     * Action goes here
+     */
+    public void signup(){
+        signupbutton.click();
+    }
+    public void login(){
+        loginbutton.click();
+    }
+    public void signout(){
+        signoutbutton.click();
+    }
 }
