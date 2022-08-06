09bdac090a21779f05512c5c3361490485a9a99b
mahmudur-sebpo <mahmudur.rahman@sebpo.com>
2022-08-06 20:05:19 +0600

    js report added

--- test-output-thread/report.js
+++ test-output-thread/report.js
@@ -1,4 +1,4 @@
 $(document).ready(function() {
-CucumberHTML.timelineItems.pushArray([{"id":"b5c152f9-f1ca-4ff5-9a42-6ae941e6054e","feature":"This is Sign up Feature","scenario":"2nd user Sign up","start":1659793390284,"group":1,"content":"","tags":"","end":1659793406268,"className":"passed"},{"id":"32bf7ae4-9a93-4a53-8236-cb218c448cf6","feature":"This is Sign up Feature","scenario":"1st user Sign up","start":1659793367755,"group":1,"content":"","tags":"","end":1659793390272,"className":"passed"}]);
+CucumberHTML.timelineItems.pushArray([{"id":"9913c7fe-56dc-499b-90f7-5143229579c5","feature":"This is Sign up Feature","scenario":"2nd user Sign up","start":1659794515175,"group":1,"content":"","tags":"","end":1659794543248,"className":"passed"},{"id":"0f91c221-850d-404a-b044-676bf2ca952e","feature":"This is Sign up Feature","scenario":"1st user Sign up","start":1659794486901,"group":1,"content":"","tags":"","end":1659794515142,"className":"passed"}]);
 CucumberHTML.timelineGroups.pushArray([{"id":1,"content":"Thread[main,5,main]"}]);
 });
