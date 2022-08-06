c13155b8f91e256394f1141eab7883078d2969b2
mahmudur-sebpo <mahmudur.rahman@sebpo.com>
2022-08-06 19:55:42 +0600

    rep js added

---
+++ test-output-thread/report.js
@@ -0,0 +1,4 @@
+$(document).ready(function() {
+CucumberHTML.timelineItems.pushArray([{"id":"b5c152f9-f1ca-4ff5-9a42-6ae941e6054e","feature":"This is Sign up Feature","scenario":"2nd user Sign up","start":1659793390284,"group":1,"content":"","tags":"","end":1659793406268,"className":"passed"},{"id":"32bf7ae4-9a93-4a53-8236-cb218c448cf6","feature":"This is Sign up Feature","scenario":"1st user Sign up","start":1659793367755,"group":1,"content":"","tags":"","end":1659793390272,"className":"passed"}]);
+CucumberHTML.timelineGroups.pushArray([{"id":1,"content":"Thread[main,5,main]"}]);
+});
